1a6f24b279bd17a693dc02deb668b8659983280f
daskiro514 <luckybuzstar@gmail.com>
2021-11-19 04:12:30 +0900

    first commit on v2.0

---
+++ routes/api/message.js
@@ -0,0 +1,147 @@
+const express = require('express')
+const router = express.Router()
+
+const Message = require('../../models/Message')
+const User = require('../../models/User')
+
+router.post('/addNewMessage', async (req, res) => {
+  const newMessage = new Message({
+    ...req.body
+  })
+
+  const client = await User.findById(req.body.client)
+  const toClientMessages = client.toClientMessages
+  const toAdminMessages = client.toAdminMessages
+  const toClientUnread = client.toClientUnread
+  const toAdminUnread = client.toAdminUnread
+
+  if (req.body.writtenBy === 'admin') {
+    await User.findByIdAndUpdate(req.body.client, { toClientMessages: toClientMessages + 1 }, { new: true })
+    if (toClientUnread >= 0) {
+      await User.findByIdAndUpdate(req.body.client, { toClientUnread: toClientUnread + 1 }, { new: true })
+    }
+  } else {
+    await User.findByIdAndUpdate(req.body.client, { toAdminMessages: toAdminMessages + 1 }, { new: true })
+    if (toAdminUnread >= 0) {
+      await User.findByIdAndUpdate(req.body.client, { toAdminUnread: toAdminUnread + 1 }, { new: true })
+    }
+  }
+
+  await newMessage.save()
+
+  res.json({
+    success: true
+  })
+})
+
+router.get('/getMessages/:id', async (req, res) => {
+  const messages = await Message.find({ client: req.params.id }).populate('writer')
+
+  res.json({
+    success: true,
+    messages
+  })
+})
+
+router.delete('/deleteMessage', async (req, res) => {
+  var messageID = req.query['id']
+  var writtenBy = req.query['written']
+  var clientID = req.query['clientID']
+
+  await Message.findByIdAndDelete(messageID)
+
+  const client = await User.findById(clientID)
+  const toClientMessages = client.toClientMessages
+  const toAdminMessages = client.toAdminMessages
+  const toClientUnread = client.toClientUnread
+  const toAdminUnread = client.toAdminUnread
+
+  if (writtenBy === 'admin') {
+    await User.findByIdAndUpdate(clientID, { toClientMessages: toClientMessages - 1 }, { new: true })
+    await User.findByIdAndUpdate(clientID, { toClientUnread: toClientUnread - 1 }, { new: true })
+  } else {
+    await User.findByIdAndUpdate(clientID, { toAdminMessages: toAdminMessages - 1 }, { new: true })
+    await User.findByIdAndUpdate(clientID, { toAdminUnread: toAdminUnread - 1 }, { new: true })
+  }
+
+  res.json({
+    success: true
+  })
+})
+
+router.get('/messagesRead', async (req, res) => {
+  var seenBy = req.query['seen']
+  var clientID = req.query['clientID']
+  if (seenBy === 'admin') {
+    await User.findByIdAndUpdate(clientID, { toAdminUnread: 0 }, { new: true })
+  } else {
+    await User.findByIdAndUpdate(clientID, { toClientUnread: 0 }, { new: true })
+  }
+
+  res.json({
+    success: true
+  })
+})
+
+router.get('/getAdminUnreadMessages', async (req, res) => {
+  const clients = await User.find({ type: 'client' })
+  var adminUnreadMessages = []
+
+  clients.forEach(client => {
+    if (client.toAdminUnread > 0) {
+      adminUnreadMessages.push({
+        clientID: client._id,
+        unreadMessages: client.toAdminUnread,
+        name: client.firstName + ' ' + client.lastName
+      })
+    }
+  })
+
+  res.json({
+    success: true,
+    adminUnreadMessages
+  })
+})
+
+router.get('/getClientUnreadMessages/:id', async (req, res) => {
+  const client = await User.findById(req.params.id)
+  var clientUnreadMessages = client.toClientUnread
+
+  res.json({
+    success: true,
+    clientUnreadMessages
+  })
+})
+
+router.get('/getClientsMessageNumbers', async (req, res) => {
+  const clients = await User.find({ type: 'client' })
+  let clientsMessageNumbers = []
+  clients.forEach(client => {
+    clientsMessageNumbers.push({
+      clientID: client._id,
+      clientFirstName: client.firstName,
+      clientLastName: client.lastName,
+      messageNumber: client.toAdminMessages
+    })
+  })
+
+  res.json({
+    success: true,
+    clientsMessageNumbers
+  })
+})
+
+router.get('/getAdminMessageNumbers/:id', async (req, res) => {
+  const client = await User.findById(req.params.id)
+
+  let adminMessageNumbers = {
+    messageNumber: client.toClientMessages
+  }
+
+  res.json({
+    success: true,
+    adminMessageNumbers
+  })
+})
+
+module.exports = router
