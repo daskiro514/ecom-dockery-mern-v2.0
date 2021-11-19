20105a6312348db9233a10c6cc200facfc2ec251
daskiro514 <luckybuzstar@gmail.com>
2021-11-20 03:05:29 +0900

    updated

--- routes/api/message.js
+++ routes/api/message.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const config = require('config')
 
 const Message = require('../../models/Message')
 const User = require('../../models/User')
